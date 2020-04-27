1f19cbcb06b8883ade3073b4cccabfc038132ebc
Avneesh Gupta <avneesh156@gmail.com>
2020-04-28 00:42:41 +0530

    Updates

---
+++ precache-manifest.1e2d1457999dc3a3cae67108b9f96759.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe3b1de2cfa7b1e7ae12e38d5336f77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40a454685bfe09207245",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "81f299679cdb95996f4e",
+    "url": "/static/js/2.337df705.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.337df705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a454685bfe09207245",
+    "url": "/static/js/main.ac007f67.chunk.js"
+  },
+  {
+    "revision": "92793b25977cd56e106c",
+    "url": "/static/js/runtime-main.ba31e598.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
