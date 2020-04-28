b0a399fa15a1acae240d735bae2ec10998ea23f2
Avneesh Gupta <avneesh156@gmail.com>
2020-04-28 16:06:19 +0530

    Updates

---
+++ precache-manifest.eaca0cab251317d08ca657fa19d03097.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8448b7eb3cd281a997e95340cb9d0918",
+    "url": "/ticket-manager/index.html"
+  },
+  {
+    "revision": "be33603c9d5124a0e10f",
+    "url": "/ticket-manager/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "81f299679cdb95996f4e",
+    "url": "/ticket-manager/static/js/2.337df705.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticket-manager/static/js/2.337df705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be33603c9d5124a0e10f",
+    "url": "/ticket-manager/static/js/main.6ee3f6d8.chunk.js"
+  },
+  {
+    "revision": "84735a8ef65e1f4fca50",
+    "url": "/ticket-manager/static/js/runtime-main.936c4e74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ticket-manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98cd195c797bc35291cadc4cc16a4d1e.js"
+  "/ticket-manager/precache-manifest.eaca0cab251317d08ca657fa19d03097.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ticket-manager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
