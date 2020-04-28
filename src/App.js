a0fc96053e7e7b4c220195e43c0afe89340d2475
Avneesh Gupta <avneesh156@gmail.com>
2020-04-28 16:13:19 +0530

    testing deployment to gh-pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing the dummy homepage
+          Testing the dummy homepage here.
         </p>
       </header>
     </div>
