55433b11446e3f6f80313afc415f08d28d121c3b
Avneesh Gupta <avneesh156@gmail.com>
2020-04-30 13:59:00 +0530

    testing gh-actions

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing the page again with auto deploy action...
+          This is sample test for gh-actions and gh-pages
         </p>
       </header>
     </div>
