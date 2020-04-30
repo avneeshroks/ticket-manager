45967c93f4c59ba6cf773e7c7c93515515bcba6c
rupam <rupam.boruah@gmail.com>
2020-04-30 17:51:46 +0530

    Added missing packages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import { create } from "jss";
-import rtl from "jss-rtl";
 import {
   createStyles,
   jssPreset,
@@ -16,7 +15,7 @@ import { createTheme } from "theme";
 import Routes from "Routes";
 
 const history = createBrowserHistory();
-const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+const jss = create({ plugins: [...jssPreset().plugins] });
 
 const useStyles = makeStyles(() =>
   createStyles({
