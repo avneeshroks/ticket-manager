592f323cf34aeb874d9ff4f652f49b28d0ff3ac3
Avneesh Gupta <avneesh156@gmail.com>
2020-04-30 12:49:09 +0530

    testing action hooks

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing the page again...
+          Testing the page again with auto deploy action...
         </p>
       </header>
     </div>
